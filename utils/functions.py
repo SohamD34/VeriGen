f4d2efc68f7f21dddcbcced3742a5537973de684
SohamD34 <deshmukh.2@iitj.ac.in>
2024-06-16 10:36:41 +0530

    'Additional formatting'

---
+++ utils/functions.py
@@ -0,0 +1,85 @@
+from pynvml import *
+from functools import partial
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+def create_prompt_formats(sample):
+    """
+    Format various fields of the sample ('instruction','output')
+    Then concatenate them using two newline characters 
+    :param sample: Sample dictionnary
+    """
+    INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    INSTRUCTION_KEY = "### Instruct: Summarize the below conversation."
+    RESPONSE_KEY = "### Output:"
+    END_KEY = "### End"
+    
+    blurb = f"\n{INTRO_BLURB}"
+    instruction = f"{INSTRUCTION_KEY}"
+    input_context = f"{sample['dialogue']}" if sample["dialogue"] else None
+    response = f"{RESPONSE_KEY}\n{sample['summary']}"
+    end = f"{END_KEY}"
+    
+    parts = [part for part in [blurb, instruction, input_context, response, end] if part]
+
+    formatted_prompt = "\n\n".join(parts)
+    sample["text"] = formatted_prompt
+
+    return sample
+
+
+
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+def get_max_length(model):
+    conf = model.config
+    max_length = None
+    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+        max_length = getattr(model.config, length_setting, None)
+        if max_length:
+            print(f"Found max lenth: {max_length}")
+            break
+    if not max_length:
+        max_length = 1024
+        print(f"Using default max length: {max_length}")
+    return max_length
+
+
+
+def preprocess_batch(batch, tokenizer, max_length):
+    """
+    Tokenizing a batch
+    """
+    return tokenizer(
+        batch["text"],
+        max_length=max_length,
+        truncation=True,
+    )
+
+
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int,seed, dataset):
+    """Format & tokenize it so it is ready for training
+    :param tokenizer (AutoTokenizer): Model Tokenizer
+    :param max_length (int): Maximum number of tokens to emit from tokenizer
+    """
+    
+    # Add prompt to each sample
+    print("Preprocessing dataset...")
+    dataset = dataset.map(create_prompt_formats)#, batched=True)
+    
+    _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
+    dataset = dataset.map(
+        _preprocessing_function,
+        batched=True,
+        remove_columns=['id', 'topic', 'dialogue', 'summary'],
+    )
+
+    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+    dataset = dataset.shuffle(seed=seed)
+
+    return dataset
--- utils/utils.py
+++
@@ -1,7 +0,0 @@
-from pynvml import *
-
-def print_gpu_utilization():
-    nvmlInit()
-    handle = nvmlDeviceGetHandleByIndex(0)
-    info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU memory occupied: {info.used//1024**2} MB.")
